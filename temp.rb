1e4a878c81a96343c85a8e4fa096579eb7165e78
shaai <shaai.sattar@gmail.com>
2014-09-29 10:37:20 -0400

    submittng quiz

--- temp.rb
+++ temp.rb
@@ -10,7 +10,11 @@ require 'pry'
 # class with a given temperature.
 # This class shouldn't contain anything else yet.
 
-
+class Celsius
+  def initialize(temperature)
+  @temperature = temperature
+  end
+end
 
 
 
@@ -28,7 +32,16 @@ require 'pry'
 # The formula is F = (C * 1.8) + 32
 
 
+class Celsius
+  def initialize(temperature)
+  @temperature = temperature
+  end
 
+  def to_fahrenheit
+    @temperature = (@temperature * 1.8) + 32
+  end
+
+end
 
 
 
@@ -47,7 +60,23 @@ require 'pry'
 # Otherwise, return "Seems perfectly fine to me."
 
 
+class Celsius
+  def initialize(temperature)
+  @temperature = temperature
+  end
 
+  def to_fahrenheit
+    @temperature = (@temperature * 1.8) + 32
+  end
+
+  def is_hot?
+    if @temperature >= 40
+      "It's hotter than a jalapeno!"
+    else
+      "Seems perfectly fine to me."
+  end
+
+end
 
 
 
@@ -66,7 +95,27 @@ require 'pry'
 # in Question 2.
 
 
+class Celsius
+  def initialize(temperature)
+  @temperature = temperature
+  end
 
+  def to_fahrenheit
+    @temperature = (temperature * 1.8) + 32
+  end
+
+  def is_hot?
+    if @temperature >= 40
+      "It's hotter than a jalapeno!"
+    else
+      "Seems perfectly fine to me."
+  end
+
+  def report
+    "The temperature is #{@temperature} Celsius or #{to_fahrenheit} Fahrenheit."
+  end
+
+end
 
 
 
@@ -81,7 +130,8 @@ require 'pry'
 # Call the is_hot? method on that instance. Don't use puts.
 
 
-
+temp_in_room = Celsius.new(22)
+temp_in_room.is_hot?
 
 
 
