0a744dc7592a3142b569a41d87c0bdfde146782e
cxoltero <carlosh95@gmail.com>
2014-09-29 10:38:46 -0400

    I think i got 90%of the code right, didnt work tho

--- temp.rb
+++ temp.rb
@@ -6,6 +6,13 @@ require 'pry'
 
 # Question 1
 # Create a class called Celsius.
+
+class Celsius
+  def initialize(temperature)
+    @temperature = temperature
+  end
+
+end
 # You should be able to initialize a new instance of this
 # class with a given temperature.
 # This class shouldn't contain anything else yet.
@@ -22,6 +29,19 @@ require 'pry'
 
 # Question 2
 # Paste your class definition from Question 1.
+class Celsius
+  def initialize(temperature)
+    @temperature = temperature
+  end
+
+  def to_farenheit(temperature_in_celsius)
+    f = (temperature_in_celsius * 1.8) + 32
+    p f
+  end
+
+end
+
+
 # Write an instance method for this class.
 # This method is called to_fahrenheit. It takes a temperature
 # in Celsius as an argument, and returns that temperature in Fahrenheit.
@@ -40,6 +60,24 @@ require 'pry'
 
 # Question 3
 # Paste your answer from Question 2 below.
+class Celsius
+  def initialize(temperature)
+    @temperature = temperature
+  end
+
+  def to_farenheit(temperature_in_celsius)
+    f = (temperature_in_celsius * 1.8) + 32
+    p f
+  end
+
+  def is_hot(temperature_in_celsius)
+    if temperature_in_celsius >= 40
+      p "It's hotter than a jalapeño!"
+    else
+      p "Seems perfectly fine to me."
+  end
+
+end
 # Write another instance method for this class.
 # This method is called is_hot?. It takes a temperature
 # in Celsius as an argument. If the temperature is more than or equal
@@ -57,6 +95,27 @@ require 'pry'
 
 # Question 4
 # Paste your answer from Question 3 below.
+class Celsius
+  def initialize(temperature)
+    @temperature = temperature
+  end
+
+  def to_farenheit(temperature_in_celsius)
+    f = (temperature_in_celsius * 1.8) + 32
+    p f
+  end
+
+  def is_hot(temperature_in_celsius)
+    if temperature_in_celsius >= 40
+      p "It's hotter than a jalapeño!"
+    else
+      p "Seems perfectly fine to me."
+  end
+
+  def report(temperature_in_celsius)
+    p "The temperature is #{temperature_in_celsius} Celsius or #{to_farenheit} Fahrenheit"
+  end
+end
 # Write another instance method for this class.
 # This method is called report. It takes a temperature
 # in Celsius as an argument and returns a sentence with that
@@ -77,6 +136,8 @@ require 'pry'
 
 # Question 5
 # Create a new instance of the Celsius class
+temp_in_room = Celsius.new
+temp_in_room.is_hot
 # named temp_in_room with temperature 22.
 # Call the is_hot? method on that instance. Don't use puts.
 
