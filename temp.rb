d5e39d92db0370830cf00f875a6b8f822ff02b96
cxoltero <carlosh95@gmail.com>
2014-09-29 11:13:39 -0400

    got 1 out of 5, but i got most of the code right, just variable mistakes

--- temp.rb
+++ temp.rb
@@ -7,12 +7,18 @@ require 'pry'
 # Question 1
 # Create a class called Celsius.
 
-class Celsius
-  def initialize(temperature)
-    @temperature = temperature
-  end
 
-end
+
+
+# class Celsius
+#   def initialize(temperature)
+#     @temperature = temperature
+#   end
+
+# end
+
+
+
 # You should be able to initialize a new instance of this
 # class with a given temperature.
 # This class shouldn't contain anything else yet.
@@ -29,17 +35,20 @@ end
 
 # Question 2
 # Paste your class definition from Question 1.
-class Celsius
-  def initialize(temperature)
-    @temperature = temperature
-  end
 
-  def to_farenheit(temperature_in_celsius)
-    f = (temperature_in_celsius * 1.8) + 32
-    p f
-  end
 
-end
+
+# class Celsius
+#   def initialize(temperature)
+#     @temperature = temperature
+#   end
+
+#   def to_farenheit
+#     f = (temperature_in_celsius * 1.8) + 32
+#     p f
+#   end
+
+# end
 
 
 # Write an instance method for this class.
@@ -60,24 +69,31 @@ end
 
 # Question 3
 # Paste your answer from Question 2 below.
-class Celsius
-  def initialize(temperature)
-    @temperature = temperature
-  end
 
-  def to_farenheit(temperature_in_celsius)
-    f = (temperature_in_celsius * 1.8) + 32
-    p f
-  end
 
-  def is_hot(temperature_in_celsius)
-    if temperature_in_celsius >= 40
-      p "It's hotter than a jalapeño!"
-    else
-      p "Seems perfectly fine to me."
-  end
 
-end
+# class Celsius
+#   def initialize(temperature)
+#     @temperature = temperature
+#   end
+
+#   def to_farenheit
+#     f = (temperature_in_celsius * 1.8) + 32
+#     p f
+#   end
+
+#   def is_hot
+#     if temperature_in_celsius >= 40
+#       p "It's hotter than a jalapeño!"
+#     else
+#       p "Seems perfectly fine to me."
+#     end
+#   end
+
+# end
+
+
+
 # Write another instance method for this class.
 # This method is called is_hot?. It takes a temperature
 # in Celsius as an argument. If the temperature is more than or equal
@@ -95,27 +111,34 @@ end
 
 # Question 4
 # Paste your answer from Question 3 below.
-class Celsius
-  def initialize(temperature)
-    @temperature = temperature
-  end
 
-  def to_farenheit(temperature_in_celsius)
-    f = (temperature_in_celsius * 1.8) + 32
-    p f
-  end
 
-  def is_hot(temperature_in_celsius)
-    if temperature_in_celsius >= 40
-      p "It's hotter than a jalapeño!"
-    else
-      p "Seems perfectly fine to me."
-  end
 
-  def report(temperature_in_celsius)
-    p "The temperature is #{temperature_in_celsius} Celsius or #{to_farenheit} Fahrenheit"
-  end
-end
+          # class Celsius
+            # def initialize(temperature)
+             # @temperature = temperature.to_f
+            # end
+
+            # def to_farenheit
+             # f = (temperature_in_celsius * 1.8) + 32
+             # p f
+            # end
+
+            # def is_hot
+              # if temperature_in_celsius >= 40
+               # p "It's hotter than a jalapeño!"
+              # else
+               # p "Seems perfectly fine to me."
+              # end
+            # end
+
+            # def report
+              # p "The temperature is #{temperature_in_celsius} Celsius or #{to_farenheit} Fahrenheit"
+            # end
+          # end
+
+
+
 # Write another instance method for this class.
 # This method is called report. It takes a temperature
 # in Celsius as an argument and returns a sentence with that
@@ -136,14 +159,142 @@ end
 
 # Question 5
 # Create a new instance of the Celsius class
-temp_in_room = Celsius.new
-temp_in_room.is_hot
+
+                  # temp_in_room = Celsius.new(22)
+
+                  # temp_in_room.is_hot
+
+
 # named temp_in_room with temperature 22.
 # Call the is_hot? method on that instance. Don't use puts.
+# Paste your answer from Question 3 below.
+
+#question 1
+
+class Celsius
+  def initializer(temp_in_c)
+    @temp_in_c = temp_in_c
+  end
+end
 
 
 
+#question 2
+
+class Celsius
+  def initializer(temp_in_c)
+    @temp_in_c = temp_in_c
+  end
+
+  def to_fahrenheit
+    f = (@temp_in_c * 1.8) + 32
+  end
+end
+
+#question 3
+
+
+class Celsius
+  def initializer(temp_in_c)
+    @temp_in_c = temp_in_c
+  end
+
+  def to_fahrenheit
+    f = (@temp_in_c * 1.8) + 32
+  end
+
+  def is_hot
+    if @temp_in_c >= 40
+      p "It's hotter than a jalapeño!"
+    else
+      p "Seems perfectly fine to me."
+    end
+  end
+end
+
+
+#question 4
+
+
+class Celsius
+  def initializer(temp_in_c)
+    @temp_in_c = temp_in_c
+  end
+
+  def to_fahrenheit
+    f = (@temp_in_c * 1.8) + 32
+  end
+
+  def is_hot
+    if @temp_in_c >= 40
+      p "It's hotter than a jalapeño!"
+    else
+      p "Seems perfectly fine to me."
+    end
+  end
+
+    def report
+    p "The temperature is #{@temp_in_c} Celsius or #{to_fahrenheit} Fahrenheit"
+  end
+end
+
+
+#question 5
+
+
+class Celsius
+  def initializer(temp_in_c)
+    @temp_in_c = temp_in_c
+  end
+
+  def to_fahrenheit
+    f = (@temp_in_c * 1.8) + 32
+  end
+
+  def is_hot
+    if @temp_in_c >= 40
+      p "It's hotter than a jalapeño!"
+    else
+      p "Seems perfectly fine to me."
+    end
+  end
+
+    def report
+    p "The temperature is #{@temp_in_c} Celsius or #{@to_fahrenheit} Fahrenheit"
+  end
+end
+
+
+                  temp_in_room = Celsius.new(22)
+
+                  temp_in_room.is_hot
+
+
+##############################################################
+
+# class Celsius
+#   def initialize(temperature)
+#     @temperature = temperature.to_f
+#   end
+
+#   def to_farenheit
+#     f = (temperature_in_celsius * 1.8) + 32
+#     p f
+#   end
+
+#   def is_hot
+#     if temperature_in_celsius >= 40
+#       p "It's hotter than a jalapeño!"
+#     else
+#       p "Seems perfectly fine to me."
+#     end
+#   end
+
+#   def report
+#     p "The temperature is #{temperature_in_celsius} Celsius or #{to_farenheit} Fahrenheit"
+#   end
+# end
 
 
 
-
+binding.pry
