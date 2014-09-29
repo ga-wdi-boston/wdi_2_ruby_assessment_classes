00b608f10653fffa483f5f38d0475744def59c2c
tsyan <anna.tsykalova@gmail.com>
2014-09-29 10:36:46 -0400

    Fixed description of arguments

--- temp.rb
+++ temp.rb
@@ -23,8 +23,8 @@ require 'pry'
 # Question 2
 # Paste your class definition from Question 1.
 # Write an instance method for this class.
-# This method is called to_fahrenheit. It takes a temperature
-# in Celsius as an argument, and returns that temperature in Fahrenheit.
+# This method is called to_fahrenheit. It returns the
+# temperature in Fahrenheit.
 # The formula is F = (C * 1.8) + 32
 
 
@@ -41,9 +41,9 @@ require 'pry'
 # Question 3
 # Paste your answer from Question 2 below.
 # Write another instance method for this class.
-# This method is called is_hot?. It takes a temperature
-# in Celsius as an argument. If the temperature is more than or equal
-# to 40, the method should return "It's hotter than a jalapeno!"
+# This method is called is_hot?. If the temperature
+# is more than or equal to 40, the method should return
+# "It's hotter than a jalapeno!"
 # Otherwise, return "Seems perfectly fine to me."
 
 
@@ -58,8 +58,7 @@ require 'pry'
 # Question 4
 # Paste your answer from Question 3 below.
 # Write another instance method for this class.
-# This method is called report. It takes a temperature
-# in Celsius as an argument and returns a sentence with that
+# This method is called report. It returns a sentence with that
 # temperature in both formats, like:
 # "The temperature is 0 Celsius or 32 Fahrenheit."
 # This method should use the to_fahrenheit method that you wrote
