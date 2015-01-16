8b1ae47f5b3c878aa5331fd206ea48cd6b4d9936
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-01-16 09:46:42 -0500

    Add Class assessment

--- lib/class_assessment.rb
+++ lib/class_assessment.rb
@@ -1,1 +1,27 @@
-# your code goes here
+class Celsius
+
+  def initialize(num)
+    @num = num
+  end
+
+  def num
+    @num
+  end
+
+  def to_fahrenheit
+    @num * 1.8 + 32
+  end
+
+  def is_hot?
+    if @num >= 40
+      "It's hotter than a jalapeno!"
+    else
+      "Seems perfectly fine to me."
+    end
+  end
+
+  def report
+    "The temperature is #{@num} Celsius or #{to_fahrenheit} Fahrenheit."
+  end
+
+end
