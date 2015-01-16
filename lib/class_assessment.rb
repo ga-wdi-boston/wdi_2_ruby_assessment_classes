82d9cacb28b1dbdf01eaaad700505335c2751cad
pat <patrick.h.needham@gmail.com>
2015-01-16 10:12:22 -0500

    changed all class methods to call instance variables

--- lib/class_assessment.rb
+++ lib/class_assessment.rb
@@ -1,10 +1,6 @@
 class Celsius
-  def initialize(temperature)# temp1, temp2, temp3, temp4)
+  def initialize(temperature)
     @temperature = temperature
-    # @temp1 = temp1
-    # @temp2 = temp2
-    # @temp3 = temp3
-    # @temp4 = temp4
   end
 
   def num
@@ -12,11 +8,11 @@ class Celsius
   end
 
   def to_fahrenheit
-    @temperature * 1.8 + 32
+    num * 1.8 + 32
   end
 
   def is_hot?
-    if @temperature >= 40
+    if num >= 40
       "It's hotter than a jalapeno!"
     else
       "Seems perfectly fine to me."
