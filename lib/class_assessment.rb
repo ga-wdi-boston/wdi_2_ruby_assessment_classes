29a2ff1bccb542b84e4bd05fc3cb0e66eed3f195
Dan Stoppelman <dstop75@gmail.com>
2015-01-16 09:40:22 -0500

    Submitted for your approval

--- lib/class_assessment.rb
+++ lib/class_assessment.rb
@@ -1,1 +1,29 @@
 # your code goes here
+class Celsius
+  def initialize(temp)
+    @temp = temp
+  end
+
+  def num
+    @temp
+  end
+
+  def to_fahrenheit
+    @temp * 9 / 5.0 + 32
+  end
+
+  def is_hot?
+    if @temp >= 40
+      return "It's hotter than a jalapeno!"
+    else
+      "Seems perfectly fine to me."
+    end
+  end
+
+  def report
+    "The temperature is #{num} Celsius or #{self.to_fahrenheit} Fahrenheit."
+  end
+
+
+
+end
