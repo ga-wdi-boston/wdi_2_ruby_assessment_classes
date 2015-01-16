199f8a7cc22994c9cb7a3b12de7a8537179b12ca
Kevin Whelan <Kevinwhelan2098@gmail.com>
2015-01-16 09:51:37 -0500

    Add Celsius class and methods

--- lib/class_assessment.rb
+++ lib/class_assessment.rb
@@ -1,1 +1,30 @@
 # your code goes here
+class Celsius
+  def initialize(temp)
+  end
+
+def temp
+  @temp1
+  @temp2
+  @temp3
+  @temp4
+end
+
+
+def to_fahrenheit
+  (@temp * 1.8) + 32
+end
+
+def is_hot?
+  @temp >= 40
+  "It's hotter than a jalapeno!"
+end
+
+def report
+  @temp
+  "The temperature is #{@temp} Celsius or #{(@temp * 1.8) + 32}."
+end
+
+
+
+end
