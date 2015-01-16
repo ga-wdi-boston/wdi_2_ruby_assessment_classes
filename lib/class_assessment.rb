a20ceaa4fbc7b166c64c4bbb45f2297d8bdebacd
Doug <djmacdonald@uchicago.edu>
2015-01-16 09:30:47 -0500

    Solutions to quiz

--- lib/class_assessment.rb
+++ lib/class_assessment.rb
@@ -1,1 +1,24 @@
 # your code goes here
+class Celsius
+
+  def initialize(temp)
+    @temp = temp
+  end
+
+  def num
+    @temp
+  end
+
+  def to_fahrenheit
+    num * 1.8 + 32
+  end
+
+  def is_hot?
+    num >= 40 ? "It's hotter than a jalapeno!" : "Seems perfectly fine to me."
+  end
+
+  def report
+    "The temperature is #{num} Celsius or #{to_fahrenheit} Fahrenheit."
+  end
+
+end
