66bf060187eb7f72b27faf499d54912f08d473cf
Annie <anne.bourgeois1@gmail.com>
2015-01-16 09:34:31 -0500

    Added a Celsius class with a bunch of methods.

--- lib/class_assessment.rb
+++ lib/class_assessment.rb
@@ -1,1 +1,30 @@
 # your code goes here
+class Celsius
+
+  def initialize(num)
+    @num = num
+  end
+
+
+
+  def num
+    @num
+  end
+
+  def to_fahrenheit
+    @fahrenheit = (num * 1.8) + 32
+  end
+
+  def is_hot?
+    if num >= 40
+      "It's hotter than a jalapeno!"
+    else
+      "Seems perfectly fine to me."
+    end
+  end
+
+  def report
+    "The temperature is #{num} Celsius or #{@fahrenheit} Fahrenheit."
+  end
+
+end
