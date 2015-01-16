56f92fcb6e2f1528919608f0ef9e09d10a8c8be5
J-Weeks <weeksjasons@gmail.com>
2015-01-16 10:14:48 -0500

    Corrected wrong answers

--- lib/class_assessment.rb
+++ lib/class_assessment.rb
@@ -1,50 +1,27 @@
 # your code goes here
 class Celsius
-  def initialize(celsius)
-    @temp1
-    @temp2
-    @temp3
-    @temp4
+  def initialize(temp)
+    @temp = temp
   end
-  # def(
-  #   @temp1
-  #   @temp2
-  #   @temp3
-  #   @temp4
 
-  # end
+  def num
+   @temp
+  end
+
   def to_fahrenheit
-    (@temp1.to_i * 1.8) +32
-    (@temp2.to_i  * 1.8) +32
-    (@temp3.to_i  * 1.8) +32
-    (@temp4.to_i  * 1.8) +32
+    (@temp.to_i * 1.8) +32
   end
 
   def is_hot?
-    if @temp1.to_i >= 40
+    if @temp.to_i >= 40
        "It's hotter than a jalapeno!"
     else
        "Seems perfectly fine to me."
     end
+  end
 
-    if @temp2.to_i >= 40
-       "It's hotter than a jalapeno!"
-    else
-       "Seems perfectly fine to me."
-    end
-
-    if @temp3.to_i >= 40
-       "It's hotter than a jalapeno!"
-    else
-       "Seems perfectly fine to me."
-    end
-
-    if @temp4.to_i >= 40
-       "It's hotter than a jalapeno!"
-    else
-       "Seems perfectly fine to me."
-    end
-
+  def report
+    "The temperature is #{@temp} Celsius or #{to_fahrenheit} Fahrenheit."
   end
 
 end
