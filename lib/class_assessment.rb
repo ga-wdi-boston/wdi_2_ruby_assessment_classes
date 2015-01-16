23892155e7aa5a07e5905653661888dc377a3021
Dan Stoppelman <dstop75@gmail.com>
2015-01-16 10:15:25 -0500

    Refactoring

--- lib/class_assessment.rb
+++ lib/class_assessment.rb
@@ -13,18 +13,19 @@ class Celsius
   end
 
   def is_hot?
-    if @temp >= 40
-      return "It's hotter than a jalapeno!"
-    else
-      "Seems perfectly fine to me."
-    end
+    # if @temp >= 40
+    #   "It's hotter than a jalapeno!"
+    # else
+    #   "Seems perfectly fine to me."
+    # end
     # better solution:
-    # @temp >= 40 ? "It's hotter than a jalapeno!" : "Seems perfectly fine to me."
+    @temp >= 40 ? "It's hotter than a jalapeno!" : "Seems perfectly fine to me."
   end
 
-    # calling self is unnecessary/redundant. #{to_fahrenheit} is better.
   def report
-    "The temperature is #{num} Celsius or #{self.to_fahrenheit} Fahrenheit."
+    # calling self is unnecessary/redundant. #{to_fahrenheit} is better.
+    # "The temperature is #{num} Celsius or #{self.to_fahrenheit} Fahrenheit."
+    "The temperature is #{num} Celsius or #{to_fahrenheit} Fahrenheit."
   end
 
 
