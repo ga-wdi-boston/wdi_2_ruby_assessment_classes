fa23df9911ca7bcfaf09d02b33757a6a1334ba89
pat <patrick.h.needham@gmail.com>
2015-01-16 09:35:01 -0500

    hotter than a jalapeno

--- lib/class_assessment.rb
+++ lib/class_assessment.rb
@@ -1,1 +1,29 @@
-# your code goes here
+class Celsius
+  def initialize(temperature)# temp1, temp2, temp3, temp4)
+    @temperature = temperature
+    # @temp1 = temp1
+    # @temp2 = temp2
+    # @temp3 = temp3
+    # @temp4 = temp4
+  end
+
+  def num
+    @temperature
+  end
+
+  def to_fahrenheit
+    @temperature * 1.8 + 32
+  end
+
+  def is_hot?
+    if @temperature >= 40
+      "It's hotter than a jalapeno!"
+    else
+      "Seems perfectly fine to me."
+    end
+  end
+
+  def report
+    "The temperature is #{num} Celsius or #{to_fahrenheit} Fahrenheit."
+  end
+end
